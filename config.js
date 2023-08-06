420f74131c95f64ae008074e4f1eb1f42533c13f
minhnohu <91530654+minhnohu@users.noreply.github.com>
2023-08-06 18:25:53 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chào em!',
+    introDesc: `Tôi hỏi chú sâu xanh,
+    Lá có gì mà thích,
+    Chú sâu cười khúc khích,
+    Thích đâu cần lí do: <3`,
+    btnIntro: 'Hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Thich em qua rui` <3.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/minhnohu' //link mess của các bạn. VD: https://m.me/minhnohu
+}
